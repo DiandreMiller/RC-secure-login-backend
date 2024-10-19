2177faddcb4c43682e2885352d9f075a0f56eb59
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-10-19 15:14:13 -0400

    functionality: added sequalize to server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Dependency
 const app = require('./app');
+const sequelize = require('./config/database');
 
 //Configuration
 require('dotenv').config();
@@ -7,7 +8,20 @@ require('dotenv').config();
 //PORT
 const PORT = process.env.PORT || 3000;
 
-//Listen
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+//Sync database and listen for requests
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been successfully established');
+        return sequelize.sync(); //Sync models with database
+    })
+
+    //Listen
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`)
+        }); 
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database:', error);
+    });
