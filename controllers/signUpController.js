1ec85c6deded9833f08c846594937fa20c21ac55
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-10-20 20:23:59 -0400

    Bug: fixed password length bug and timestamp bug

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -6,7 +6,18 @@ const { User } = require('../models');
 //Sign Up
 async function signUp(request, response) {
     try {
+        console.log('Received request:', request.body);
+        
         const { username, email, password, dateOfBirth } = request.body;
+
+        console.log('Incoming sign-up data:', { username, email, password, dateOfBirth });
+
+        //Check to see if user exists
+        const existingUser = await User.findOne({ where: { email } });
+
+        if(existingUser) {
+            return response.status(409).json({ error: 'User already exists' });
+        }
 
         //Password hashed with bcrypt
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -16,6 +27,7 @@ async function signUp(request, response) {
 
         response.status(201).json({ message: 'User created', user });
     } catch (error) {
+        console.error('Sign Up Error:', error);
         response.status(500).json({ error: error.message });
     }
 }
