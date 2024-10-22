70d7203941a8a42a07726850d0925e61b376a10c
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-10-22 11:59:56 -0400

    Added sequelize database

--- config/database.js
+++ config/database.js
@@ -1,9 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432
 });
+
+if(process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        port: process.env.DB_PORT || 5432,
+        host: process.env.DB_HOST || 'localhost',
+    });
+}
 
 module.exports = sequelize;
